3df76497f11bdb7d42e44a9ec7d2da46997293c4
phon xay <majic42@hotmail.com>
2024-08-06 09:31:04 +0000

    change logic

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,7 +10,8 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return 0;
-	return (node->left == NULL && node->right == NULL) ||
-           (binary_tree_is_leaf(node->left) + binary_tree_is_leaf(node->right));
+		return (0);
+	return ((node->left == NULL && node->right == NULL) ||
+		(binary_tree_is_leaf(node->left) +
+		 binary_tree_is_leaf(node->right)));
 }
