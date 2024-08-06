f70eebd9cb2058d9bf4e2ec791366f7cdeab0744
phon xay <majic42@hotmail.com>
2024-08-06 09:43:04 +0000

    change logic

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,17 +1,17 @@
-#include <stdlib.h>
+#include <stddef.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - find the leaf node
- * @node: a pointer to root node
- * Return: integer
-*/
-
+ * binary_tree_is_leaf - Checks if a node is a leaf in a binary tree.
+ * @node: Pointer to the node to check.
+ *
+ * Return: 1 if node is a leaf, 0 otherwise.
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	return ((node->left == NULL && node->right == NULL) ||
-		(binary_tree_is_leaf(node->left) +
-		 binary_tree_is_leaf(node->right)));
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
 }
