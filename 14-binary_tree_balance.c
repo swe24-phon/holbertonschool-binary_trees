39c103cf86f0ee30cfe853e435414c9f2a849d45
MaFlet <mfletcher2808@hotmail.com>
2024-08-07 10:23:19 +0000

    modified task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,29 +1,6 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 /**
-*binary_tree_height - measures the height of a binary tree
-*@tree: a pointer to root node
-*Return: If tree is NULL, your function must return 0
-*/
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-size_t left, right;
-if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-{
-return (0);
-}
-left = binary_tree_height(tree->left);
-right = binary_tree_height(tree->right);
-if (left > right)
-{
-return (left + 1);
-}
-else
-{
-return (right + 1);
-}
-}
-/**
 *binary_tree_balance - measures balance factor of binary
 *tree
 *@tree: a pointer to the root node of tree to measure
@@ -31,10 +8,23 @@ return (right + 1);
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-size_t left = 0, right = 0;
-if (tree == NULL)
+if (tree)
+return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 return (0);
-left = binary_tree_height(tree->left);
-right = binary_tree_height(tree->right);
-return (left - right;
+}
+/**
+*binary_tree_height - measures the height of a binary tree
+*@tree: a pointer to root node
+*Return: If tree is NULL, your function must return 0
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t left = 0, right = 0;
+if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+return (0);
+left += 1 + binary_tree_height(tree->left);
+right += 1 + binary_tree_height(tree->right);
+if (left > right)
+return (left);
+return (right);
 }
