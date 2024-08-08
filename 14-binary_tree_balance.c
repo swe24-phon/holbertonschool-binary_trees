f78ab1ecda80de0dd2f198d44ab76cbd73c87566
MaFlet <mfletcher2808@hotmail.com>
2024-08-08 22:44:38 +0000

    modified task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,9 +8,12 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-if (tree)
-return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+int left, right;
+if (tree == NULL)
 return (0);
+left = tree->left ? (int)binary_tree_height(tree->left) : -1;
+right = tree->right ? (int)binary_tree_height(tree->right) : -1;
+return (left - right);
 }
 /**
 *binary_tree_height - measures the height of a binary tree
@@ -19,12 +22,19 @@ return (0);
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left = 0, right = 0;
+size_t left, right;
 if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+{
 return (0);
-left += 1 + binary_tree_height(tree->left);
-right += 1 + binary_tree_height(tree->right);
+}
+left = binary_tree_height(tree->left);
+right = binary_tree_height(tree->right);
 if (left > right)
-return (left);
-return (right);
+{
+return (left + 1);
+}
+else
+{
+return (right + 1);
+}
 }
