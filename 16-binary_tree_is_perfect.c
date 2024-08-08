c025ffeac317714ca939af8fe52c41c85a578bde
phon xay <majic42@hotmail.com>
2024-08-08 22:13:45 +1000

    change logic

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,18 +12,18 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_height, right_height;
-    if (tree == NULL)
-        return (0);
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
-    if (tree->left == NULL || tree->right == NULL)
-        return (0);
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	size_t left_height = 0, right_height = 0;
 
-    if (left_height != right_height)
-        return (0);
-    return (binary_tree_is_perfect(tree->left) &&
-    	binary_tree_is_perfect(tree->right));
+	if (tree == NULL)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (left_height == right_height)
+	{
+		if (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right))
+		return (1);
+	}
+
+	return (0);
 }
