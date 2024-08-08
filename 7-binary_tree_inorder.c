5ead7a572231ce9de1027c6532128c04e3d87f30
phon xay <majic42@hotmail.com>
2024-08-08 19:57:49 +1000

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -12,13 +12,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-
-	// Traverse the left subtree
 	binary_tree_inorder(tree->left, func);
-
-	// Traverse the root node
 	func(tree->n);
-
-	// Traverse the right subtree
 	binary_tree_inorder(tree->right, func);
 }
